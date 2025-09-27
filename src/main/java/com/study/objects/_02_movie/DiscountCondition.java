8021ac711c6214d9fb3c58905361b9e0544a2dc2
HJ0216 <6120hj@gmail.com>
2025-09-27 10:28:17 +0900

    03 할인 요금 구하기

---
+++ src/main/java/com/study/objects/_02_movie/DiscountCondition.java
@@ -0,0 +1,6 @@
+package com.study.objects._02_movie;
+
+public interface DiscountCondition {
+
+  public boolean isSatisfiedBy(Screening screening);
+}
--- src/main/java/com/study/objects/_02_movie/Movie.java
+++ src/main/java/com/study/objects/_02_movie/Movie.java
@@ -1,13 +1,48 @@
 package com.study.objects._02_movie;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Movie {
 
-  private Money money;
+  private final String title;
+  private final Duration runningTime;
+  private final Money fee;
+  private final DiscountPolicy discountPolicy;
+
   public Money getFee() {
-    return money;
+    return fee;
   }
 
   public Money calculateMovieFee(Screening screening) {
-    return null;
+    return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+  }
+
+  public Movie getAvatar() {
+    return new Movie(
+        "Avatar",
+        Duration.ofMinutes(120),
+        Money.wons(10_000),
+        new AmountDiscountPolicy(
+            Money.wons(800),
+            new SequenceCondition(1),
+            new SequenceCondition(10),
+            new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
+            new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))));
+  }
+
+  public Movie getTitanic() {
+    return new Movie(
+        "Titanic",
+        Duration.ofMinutes(180),
+        Money.wons(11_000),
+        new PercentDiscountPolicy(
+            0.1,
+            new PeriodCondition(DayOfWeek.TUESDAY, LocalTime.of(14, 0), LocalTime.of(16, 59)),
+            new SequenceCondition(2),
+            new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(13, 59))));
   }
 }
