4764b18faa2d28eb4d73fb2238d2b73451f555d9
HJ0216 <6120hj@gmail.com>
2025-10-03 06:36:09 +0900

    05 클래스

--- src/main/java/com/study/objects/_07_salaries/Employee.java
+++ src/main/java/com/study/objects/_07_salaries/Employee.java
@@ -1,12 +1,18 @@
 package com.study.objects._07_salaries;
 
 import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public record Employee(
-  String name,
-  BigDecimal basePay,
-  boolean hourly,
-  BigDecimal workingHours
-) {
+@Getter
+@RequiredArgsConstructor
+public abstract class Employee {
+  private final String name;
+  private final BigDecimal basePay;
+  private final boolean hourly;
+  private final BigDecimal workingHours;
+
+  public abstract BigDecimal getTaxRate();
+  public abstract BigDecimal getMonthlyPay();
 
 }
--- src/main/java/com/study/objects/_07_salaries/Salary.java
+++ src/main/java/com/study/objects/_07_salaries/Salary.java
@@ -9,35 +9,17 @@ public class Salary {
 
   private final List<Employee> employees = new ArrayList<>(
       Arrays.asList(
-          new Employee("Employee1", BigDecimal.valueOf(400), false, BigDecimal.ZERO),
-          new Employee("Employee2", BigDecimal.valueOf(300), false, BigDecimal.ZERO),
-          new Employee("Employee3", BigDecimal.valueOf(250), false, BigDecimal.ZERO),
-          new Employee("PartTime1", BigDecimal.valueOf(1), true, BigDecimal.valueOf(120)),
-          new Employee("PartTime2", BigDecimal.valueOf(1), true, BigDecimal.valueOf(120)),
-          new Employee("PartTime3", BigDecimal.valueOf(1.5), true, BigDecimal.valueOf(100))
+          new SalariedEmployee("Employee1", BigDecimal.valueOf(400)),
+          new SalariedEmployee("Employee2", BigDecimal.valueOf(300)),
+          new SalariedEmployee("Employee3", BigDecimal.valueOf(250)),
+          new HourlyEmployee("PartTime1", BigDecimal.valueOf(1), BigDecimal.valueOf(120)),
+          new HourlyEmployee("PartTime2", BigDecimal.valueOf(1), BigDecimal.valueOf(120)),
+          new HourlyEmployee("PartTime3", BigDecimal.valueOf(1.5), BigDecimal.valueOf(100))
       )
   );
 
   public SalaryResponse calculatePay(Employee employee) {
-    BigDecimal taxRate = getTaxRate(employee.hourly());
-    BigDecimal pay = calculatePay(employee, taxRate);
-    return toResponse(employee.name(), pay);
-  }
-
-  private BigDecimal calculatePay(Employee employee, BigDecimal taxRate) {
-    if (isHourly(employee.name())) {
-      return calculatePartTimePay(employee, taxRate);
-    }
-
-    return calculateEmployeePay(employee, taxRate);
-  }
-
-  private boolean isHourly(String name) {
-    return employees.stream()
-                    .filter(e -> e.name().equals(name))
-                    .map(Employee::hourly)
-                    .findFirst()
-                    .orElse(false);
+    return toResponse(employee.getName(), employee.getMonthlyPay());
   }
 
   public SalaryResponse totalBasePay() {
@@ -45,13 +27,28 @@ public class Salary {
     return toResponse("TOTAL", sum);
   }
 
+  public SalaryResponse totalMonthlyPay() {
+    BigDecimal sum = sumMonthlyPay();
+    return toResponse("TOTAL", sum);
+  }
+
   private BigDecimal sumBasePay() {
     BigDecimal sum = BigDecimal.ZERO;
 
     for (Employee employee : employees) {
-      if (!employee.hourly()) {
-        sum = sum.add(employee.basePay());
+      if (!employee.isHourly()) {
+        sum = sum.add(employee.getBasePay());
       }
+    }
+
+    return sum;
+  }
+
+  private BigDecimal sumMonthlyPay() {
+    BigDecimal sum = BigDecimal.ZERO;
+
+    for (Employee employee : employees) {
+      sum = sum.add(employee.getMonthlyPay());
     }
 
     return sum;
@@ -59,21 +56,5 @@ public class Salary {
 
   private SalaryResponse toResponse(String name, BigDecimal pay) {
     return new SalaryResponse(name, pay);
-  }
-
-  private BigDecimal calculateEmployeePay(Employee employee, BigDecimal taxRate) {
-    BigDecimal pay = employee.basePay();
-
-    return pay.multiply(BigDecimal.ONE.subtract(taxRate));
-  }
-
-  private BigDecimal calculatePartTimePay(Employee employee, BigDecimal taxRate) {
-    BigDecimal pay = employee.basePay().multiply(employee.workingHours());
-
-    return pay.multiply(BigDecimal.ONE.subtract(taxRate));
-  }
-
-  private BigDecimal getTaxRate(boolean hourly) {
-    return hourly ? BigDecimal.ZERO : BigDecimal.TEN;
   }
 }
